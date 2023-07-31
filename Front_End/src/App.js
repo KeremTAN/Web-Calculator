19c5b975186f41d2f8ce90397ce94c23decd3c0f
KeremTAN <keremtan.95@hotmail.com>
2023-07-31 05:00:01 +0300

    calculator-panel is written

---
+++ Front_End/src/App.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+import './App.css';
+import CalculatorPanel from './CalculatorPanel';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CalculatorPanel />
+      </header>
+    </div>
+  );
+}
+
+export default App;
